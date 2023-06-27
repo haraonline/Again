33e90701ea32215adbf756a7aae13f42e9f43cf5
haraonline <haragopal.kovvali@gmail.com>
2023-06-27 19:58:35 +0200

    if else variations

---
+++ main.py
@@ -0,0 +1,29 @@
+
+# control structures
+# if, elif, else
+
+# if <condition>:
+#     <code>
+# elif <condition>:
+#     <code>
+# else:
+#     <code>
+
+number = int(input("Enter any number from 0 to 10 : "))
+if number < 10:
+    print("Number is less than 10")
+elif number > 10:
+    print("Number is greater than 10")
+else:
+    print("Number is equal to 10")
+
+# oneline if statement
+# if <condition>: <code> else: <code>
+# if <condition>: <code> elif <condition>: <code> else: <code>
+
+# if number < 10: print("Number is less than 10")
+# elif number > 10: print("Number is greater than 10")
+
+if number < 10: print("Number is less than 10") elif number > 10: print("Number is greater than 10") else: print("Number is equal to 10")
+
+
