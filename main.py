363df4f401fb11d5a9477ae807414fce31d32328
haraonline <haragopal.kovvali@gmail.com>
2023-06-28 17:38:36 +0200

    while, break, continue

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 
-# control structures
-# if, elif, else
-
-# if <condition>:
-#     <code>
-# elif <condition>:
-#     <code>
-# else:
-#     <code>
-
-number = int(input("Enter any number from 0 to 10 : "))
-# if number < 10:
-#     print("Number is less than 10")
-# elif number > 10:
-#     print("Number is greater than 10")
-# else:
-#     print("Number is equal to 10")
-
-# oneline if statement
-# if <condition>: <code> else: <code>
-# if <condition>: <code> elif <condition>: <code> else: <code>
-
-# if number < 10: print("Number is less than 10")
-# elif number > 10: print("Number is greater than 10")
-
-# oneline if elif else statement for the number variable
-print("Number is less than 10") if number < 10 else print("Number is greater than 10") if number > 10 else print("Number is equal to 10")
 
 
+# how does while loop works
+# while loop is a conditional loop
+# it will keep running until the condition is true
+# once the condition is false, it will stop running
+
+# i = 0
+# while i < 10:
+#     print(i < 10)
+#     i += 1
+# print("--------------------")
+
+# password = "pass123"
+# user_input = ""
+# counter = 0
+
+# while user_input != password and counter < 3:
+#     # if counter == 3:
+#     #     print("You have reached maximum number of tries")
+#     #     break
+#     user_input = input("Enter your password: ")
+#     counter += 1
+
+# # print("You are logged in")
 
 
-
+while True:
+    name = input("Enter your name: ")
+    if name != "hara":
+        continue
+    else:
+        break
+print("You are logged in")
