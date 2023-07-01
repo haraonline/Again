5234d6369bad784b7ad145a6ce9e8e561ae0182b
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-07-01 22:03:39 +0200

    lists and tuples

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
-# what is for loop
-# for loop is used to iterate over a sequence (list, tuple, string) or other iterable objects
-# Iterating over a sequence is called traversal
-# break and continue can be used inside for loop
+
+# lists are mutable (elements can be changed i.e, added, removed, or replaced)
+# tuples are immutable (elements cannot be changed i.e, added, removed, or replaced)
+# lists are defined using square brackets []
+# tuples are defined using parentheses ()
+# lists are slower than tuples
+# the performance is negligible for small lists and tuples
+# importance difference is mutability
+# if you wish to store just one element in parentheses, you need a trailing comma
 
 
-for element in [1, 2, 3, 4, 5]:
-    print(element)
 
-print("....................")
-
-for element in "Hello World!":
-    print(element)
-
-print("....................")
-
-for element in range(6): # range(1, 6)
-    print(element)
-print("....................")
+# example of a list
+list_of_numbers = [1, 2, 3, 4, 5]
+print(list_of_numbers[2])
+print(list_of_numbers)
 
 
-for element in range(1, 20, 3):
-    print(element)
-print("....................")
+# example of a tuple
+tuple_of_numbers = (1, 2, 3, 4, 5)
+print(tuple_of_numbers[3])
+print(tuple_of_numbers)
 
-# range can also go in the reverse direction
+# example of a tuple with one element
+tuple_of_numbers = (1,)
 
+# unpacking a tuple
+person = ("John", "Doe", 53)
+print(person)
+first_name, last_name, age = person # unpacking a tuple
+print(first_name)
+
+# or normally
+person = ("John", "Doe", 53)
+first_name = person[0]
+last_name = person[1]
+age = person[2]
+
+
+# return a tuple from a function
+def get_person():
+    name = "John"
+    age = 53
+    country = "USA"
+    return name, age, country
+
+person = get_person()
+print(person)
